ba2b9f367ba2fa7755b6470e52e122dfbc493bbb
fourn <419571893@qq.com>
2018-03-19 18:07:59 +0800

    继续做后台统计

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -8,6 +8,10 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\Collapse;
+use Encore\Admin\Widgets\InfoBox;
+use Encore\Admin\Widgets\Table;
+use DB;
 
 class HomeController extends Controller
 {
@@ -15,25 +19,29 @@ class HomeController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('Dashboard');
-            $content->description('Description...');
-
-            $content->row(Dashboard::title());
+            $content->header('仪表盘');
 
             $content->row(function (Row $row) {
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
+                $row->column(4, Dashboard::environment());
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
+                $row->column(8, function (Column $column) {
+                    $column->row(function (Row $row){
+                        $count = DB::table('users')->count();
+                        $infoBox = new InfoBox('会员总数', 'users', 'aqua', '/admin/users', $count);
+                        $row->column(4, $infoBox->render());
+                        $count = DB::table('orders')->count();
+                        $infoBox = new InfoBox('订单总数', 'file', 'red', '/admin/orders', $count);
+                        $row->column(4, $infoBox->render());
+                        $count = DB::table('books')->count();
+                        $infoBox = new InfoBox('书本总数', 'book', 'yellow', '/admin/books', $count);
+                        $row->column(4, $infoBox->render());
+                    });
+                    $column->row('');
+                    $column->row('');
                 });
             });
+
         });
     }
 }
