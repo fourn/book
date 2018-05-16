c623dfbf04efda8b6908e1d070f841fd3df5ad55
fourn <419571893@qq.com>
2018-05-16 21:47:49 +0800

    原样输出

--- resources/views/orders/pay.blade.php
+++ resources/views/orders/pay.blade.php
@@ -44,7 +44,7 @@
     //winCloseMyWin(".winbgclick");//关闭窗口
     function jsApiCall(){
         WeixinJSBridge.invoke(
-            'getBrandWCPayRequest', {{ $json }},
+            'getBrandWCPayRequest', {!! $json !!},
             function(res){
                 if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                     // 使用以上方式判断前端返回,微信团队郑重提示：
