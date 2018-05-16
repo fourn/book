b95f3096001c777bac5a939cdac1ba968f2af876
fourn <419571893@qq.com>
2018-05-16 17:58:23 +0800

    微信支付

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -103,7 +103,7 @@ class OrdersController extends Controller
             'openid' => $order->user->openid,
         ];
         $result = $payment->order->unify($orderData);
-        dd($payment);
+        dd($result);
         $jssdk = $payment->jssdk;
         $json = $jssdk->bridgeConfig($result['prepayId']);
         return view('orders.pay', compact('order'));
