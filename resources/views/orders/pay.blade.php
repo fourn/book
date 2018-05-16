eee459f70805a7ba5217c88e2a19f6165c0efaf3
fourn <419571893@qq.com>
2018-05-16 19:06:06 +0800

    bug

--- resources/views/orders/pay.blade.php
+++ resources/views/orders/pay.blade.php
@@ -50,7 +50,7 @@
                     // 使用以上方式判断前端返回,微信团队郑重提示：
                     // res.err_msg将在用户支付成功后返回
                     // ok，但并不保证它绝对可靠。
-                    location.href="{{ route($order->userLink()) }}";
+                    location.href="{{ $order->userLink() }}";
                 }
             }
         );
