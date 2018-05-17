12db2d3d0c6ecb5419dfd2e0540302ff83fb1886
fourn <419571893@qq.com>
2018-05-17 14:24:44 +0800

    权限

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Admin;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,7 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         \Horizon::auth(function ($request) {
             // 是否后台超级管理员
-            return \Admin::user()->isAdministrator();
+            return Admin::user()->isAdministrator();
         });
         //
     }
